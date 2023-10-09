fbffa34178243fb5542d6d6c21a530ac1876ea9b
dainay <115573080+dainay@users.noreply.github.com>
2023-10-09 15:51:31 +0200

    Try with steamgraph

--- script.js
+++ script.js
@@ -41,3 +41,40 @@
             .append("path")
             .style("fill", (d, i) => color(i))
             .attr("d", area);
+
+
+
+            x = d3.scaleLinear()
+            .domain(d3.extent(data, function(d) { return d.year; }))
+            .range([ 0, width ]);
+          svg.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x).ticks(5));
+        
+          // Add Y axis
+          var y = d3.scaleLinear()
+            .domain([-100000, 100000])
+            .range([ height, 0 ]);
+          svg.append("g")
+            .call(d3.axisLeft(y));
+        
+          // color palette
+          var color = d3.scaleOrdinal()
+            .domain(keys)
+            .range(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf'])
+        
+          //stack the data?
+          var stackedData = d3.stack()
+            .offset(d3.stackOffsetSilhouette)
+            .keys(keys)
+            (data)
+        
+          // Show the areas
+          svg
+            .append("path").datum(data)
+              .style("fill", function(d) { return color(d.key); })
+              .attr("d", d3.area()
+                .x(function(d, i) { return x(d.data.year); })
+                .y0(function(d) { return y(0); })
+                .y1(function(d) { return y(d.or); })
+            )
